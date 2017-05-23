ee51ce60bed99ac7be46e501fc7f825b7b5e9612
sw9t <warfatre@gmail.com>
2017-05-23 21:51:54 +0300

    фак

--- Smo/Smo/Smo_s_neog_oc.cs
+++ Smo/Smo/Smo_s_neog_oc.cs
@@ -5,9 +5,9 @@ using System.Text;
 
 namespace SPIRI_SMO
 {
-    public class Smo_s_otkazami
+    public class Smo_s_neog_oc
     {
-        private static int factorial(int numb)
+       /* private static int factorial(int numb)
         {
             int res = 1;
             for (int i = numb; i > 1; i--)
@@ -15,16 +15,20 @@ namespace SPIRI_SMO
                 res *= i;
             }
             return res;
-        }
+        }*/
         public static double get_miu(double t_obslujivania)
         {
             return 1 / t_obslujivania;
         }
-        public static double get_ro(double t_obslujivania,double lambda)
+        public static double get_ro(double t_obslujivania, double lambda)
         {
-            return lambda*t_obslujivania;
+            return lambda * t_obslujivania;
         }
-        public static double[] get_Pi(int n_kanalov,double ro)
+        public static double get_ro_stat(double ro, double n_kanalov)
+        {
+            return ro/n_kanalov;
+        }
+        public static double[] get_Pi(int n_kanalov, double ro)
         {
             double[] array_P_i = new double[n_kanalov];
             double temp = 1;
@@ -32,6 +36,8 @@ namespace SPIRI_SMO
             {
                 temp += Math.Pow(ro, i) / factorial(i);
             }
+            double temp1 = Math.Pow(ro, n_kanalov + 1) / (factorial((int)(n_kanalov)) * (n_kanalov - ro));
+            temp += temp1;
             array_P_i[0] = 1 / temp;
             for (int i = 1; i <= n_kanalov; i++)
             {
@@ -39,39 +45,21 @@ namespace SPIRI_SMO
             }
             return array_P_i;
         }
-        public static double get_p_otkaza(double[] Pi, int n_kanalov)
+        public static double get_r(double[] Pi, int n_kanalov, double ro)
         {
-            return Pi[(int)n_kanalov];
+            return (Math.Pow(ro, n_kanalov + 1) * Pi[0]) / (n_kanalov * factorial((int)n_kanalov) * Math.Pow(1 - ro / n_kanalov, 2));
         }
-        public static double get_p_obslujivania(double p_otkaza)
-        {
-            return 1-p_otkaza;
+        public static double get_t_oj(double r, double lambda)
+        { 
+        return r/lambda;
         }
-        public static double get_A(double lambda,double P_obslujivania)
+        public static double get_t_sist(double t_oj, double t_obs)
         {
-            return lambda*P_obslujivania;
-        }
-        public static double get_n_zaneatih(double A, double t_obslujivania)
-        {
-            return A * t_obslujivania;
-        }
-        public static double get_n_svobodnih(int n_kanalov, double n_zaneatih)
-        {
-            return n_kanalov-n_zaneatih;
-        }
-        public static double get_k_zaneatih(int n_kanalov, double n_zaneatih)
-        {
-            return n_zaneatih / n_kanalov;
-        }
-        public static double get_k_prostoia(double k_zaneatih)
-        {
-            return 1-k_zaneatih;
-        }
-        public static double get_s(int n_kanalov, double g,double d)
-        {
-            return (g*n_kanalov)/d;
+            return t_obs + t_oj;
         }
 
+        
+       
 
 
 
